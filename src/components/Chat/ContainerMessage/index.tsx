14e45a7659d944023f49fb1a6b3fe37f79782248
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-05 19:58:43 -0300

    feat: alter style

--- src/components/Chat/ContainerMessage/index.tsx
+++ src/components/Chat/ContainerMessage/index.tsx
@@ -4,26 +4,16 @@ import { IoPersonCircle } from "react-icons/io5";
 export default function ContainerMessage({ author, createAt, id, text }: Message) {
     return (
         <div className="container-message" id={id}>
-            {/* icon */}
             <div className="icon">
                 <IoPersonCircle className="svg" />
             </div>
-
-            {/* body */}
             <div className="body">
-
-                {/* author */}
                 <div>
                     <h3 className="container-author">
-                        {/* author */}
                         <span className="author">{ author }</span>
-
-                        {/* date */}
                         <span className="date">{ `${createAt}` }</span>
                     </h3>
                 </div>
-
-                {/* text of message */}
                 <div>
                     <p className="text-message">
                         {text}
