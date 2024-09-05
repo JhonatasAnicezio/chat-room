bb71a780eb5e8ddf375e22819333a4448a4832f4
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-05 13:31:13 -0300

    feat: implements input in chat

--- src/components/Chat/Sender/index.tsx
+++ src/components/Chat/Sender/index.tsx
@@ -1,20 +1,33 @@
 'use client'
 import { socket } from "@/service/socket";
+import { formSendMessageSchema, FormSendMessageSchema } from "@/types/Chat/components/FormSendMessage";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export default function Sender() {
-    return (
-        <button
-        className="absolute bottom-[20px] right-[100px]"
-        onClick={() => {
-          socket.emit('message', {
-            id: `iddasala${new Date()}`,
-            text: `Texto enviado na data ${new Date()} `,
-            createAt: new Date(),
-            author: 'usuario teste',
-          });
-        }}
-      >
-        Manda mensagem
-      </button>
-    )
+  const { register, handleSubmit, reset } = useForm<FormSendMessageSchema>({
+    resolver: zodResolver(formSendMessageSchema),
+  });
+
+  const sendMessage = ({ message }: FormSendMessageSchema) => {
+    socket.emit('message', {
+      id: `iddasala${new Date()}`,
+      text: message,
+      createAt: new Date(),
+      author: 'usuario teste',
+    });
+
+    reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit(sendMessage)}>
+      <input
+        className="text-black"
+        {...register('message')}
+        type="text"
+        placeholder="Conversar com o chat"
+      />
+    </form>
+  )
 }
