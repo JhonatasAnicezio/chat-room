f6a51607701e86452c0fc3f27c904b9cc577b930
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-05 15:04:40 -0300

    feat: implements component Sender

--- src/components/Chat/Sender/index.tsx
+++ src/components/Chat/Sender/index.tsx
@@ -1,33 +1,34 @@
-'use client'
-import { socket } from "@/service/socket";
-import { formSendMessageSchema, FormSendMessageSchema } from "@/types/Chat/components/FormSendMessage";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+  'use client'
+  import { socket } from "@/service/socket";
+  import { formSendMessageSchema, FormSendMessageSchema } from "@/types/Chat/components/FormSendMessage";
+  import { zodResolver } from "@hookform/resolvers/zod";
+import { useRef } from "react";
+  import { useForm } from "react-hook-form";
 
-export default function Sender() {
-  const { register, handleSubmit, reset } = useForm<FormSendMessageSchema>({
-    resolver: zodResolver(formSendMessageSchema),
-  });
-
-  const sendMessage = ({ message }: FormSendMessageSchema) => {
-    socket.emit('message', {
-      id: `iddasala${new Date()}`,
-      text: message,
-      createAt: new Date(),
-      author: 'usuario teste',
+  export default function Sender() {
+    const { register, handleSubmit, reset } = useForm<FormSendMessageSchema>({
+      resolver: zodResolver(formSendMessageSchema),
     });
 
-    reset();
-  }
+    const sendMessage = ({ message }: FormSendMessageSchema) => {
+      socket.emit('message', {
+        id: `iddasala${new Date()}`,
+        text: message,
+        createAt: new Date(),
+        author: 'usuario teste',
+      });
 
-  return (
-    <form onSubmit={handleSubmit(sendMessage)}>
-      <input
-        className="text-black"
-        {...register('message')}
-        type="text"
-        placeholder="Conversar com o chat"
-      />
-    </form>
-  )
-}
+      reset();
+    }
+
+    return (
+      <form className="flex items-center justify-center w-full p-5" onSubmit={handleSubmit(sendMessage)}>
+        <input
+          type="text"
+          className="bg-[#383A40] focus:outline-none px-4 py-2.5 rounded-md text-[#D4D7DB] w-full resize-none custom-scrollbar"
+          {...register('message')}
+          placeholder="Conversar com o chat"
+        />
+      </form>
+    )
+  }
