f9e81d657f8fd54504654c857742b1219fc67cee
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-05 21:39:45 -0300

    feat: add style Profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,6 +1,8 @@
 import { usernameSchema, UsernameSchema } from "@/types/Profile/Username"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import './index.css';
+import { IoPersonCircle } from "react-icons/io5";
 
 interface ProfileProps {
     setUsername: (username: string) => void,
@@ -13,8 +15,16 @@ export default function Profile({ setUsername }: ProfileProps) {
 
     return (
         <div>
-            <form onSubmit={handleSubmit(({ username }) => setUsername(username))}>
+            <form
+                className="form-username"
+                onSubmit={handleSubmit(({ username }) => setUsername(username))}
+            >
+                <div className="div-icon">
+                    <IoPersonCircle className="svg" />
+                </div>
+
                 <input
+                    className="input-username"
                     {...register('username')}
                     placeholder="Insira seu nome de usuario"
                     type="text"
