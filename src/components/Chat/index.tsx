2c0f224e23ab9f12f7ab6df880f7f7ff8a4ab6ca
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-10 14:32:16 -0300

    feat: add useEffect for start-chat

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,21 +1,14 @@
-import { Message } from "postcss";
 import ContainerMessage from "./ContainerMessage";
-import { useState } from "react";
+import { useContext, useEffect } from "react";
+import { ChatContext } from "@/context/Chat/chat-context";
 import { socket } from "@/service/socket";
 
 export default function Chat() {
-    const [messages, setMessages] = useState<Message[]>([]);
+    const { messages } = useContext(ChatContext);
 
-
-    socket.on('message', (data: Message[]) => {
-        console.log('messagem enviada');
-
-        setMessages(data);
-    })
-
-    socket.on("messages", (data: Message[]) => {
-        setMessages(data);
-    })
+    useEffect(() => {
+        socket.emit('start-chat');
+    }, [])
 
     return (
         <ol className="flex flex-col-reverse gap-5 p-5 w-[1302px] h-[100%] overflow-y-auto custom-scrollbar">
