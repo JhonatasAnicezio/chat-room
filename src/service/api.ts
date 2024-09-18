37b455264c744375b97917a0c636505afd7e5bdf
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-18 11:28:42 -0300

    feat: implements singIn and register

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export const clients = axios.create({
+export const authApi = axios.create({
   baseURL: 'http://localhost:3000/authentication',
 });
---
+++ src/service/authentication/index.ts
@@ -0,0 +1,34 @@
+import { AuthEmailSchema } from "@/components/authentication/auth-email/auth-email-schema";
+import { authApi } from "../api";
+import { AxiosError, AxiosResponse } from "axios";
+
+
+export async function singIn({ email, password }: AuthEmailSchema) {
+    try {
+        const { data } = await authApi.post<AxiosResponse>('/sing-in', { email, password });
+
+        return data;
+    } catch (error: AxiosError | unknown) {
+        if(error instanceof AxiosError) {
+            const message = error.response?.data.message;
+            throw new Error(message)
+        }
+    }
+}
+
+export async function register({ email, password }: AuthEmailSchema) {
+    try {
+        const data = await authApi.post<AxiosResponse>('/', { email, password });
+
+        if(data.status !== 201) {
+            throw new Error('Erro ao tentar cadastrar')
+        }
+
+        return true
+    } catch (error: AxiosError | unknown) {
+        if(error instanceof AxiosError) {
+            const message = error.response?.data.message;
+            throw new Error(message)
+        }
+    }
+}
