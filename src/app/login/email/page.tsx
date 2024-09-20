edb809c289f174c46a93295964e5b8d96567e44a
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-20 15:46:27 -0300

    test

--- src/app/login/email/page.tsx
+++ src/app/login/email/page.tsx
@@ -1,19 +1,24 @@
-'use client'
-
 import AuthEmail from "@/components/authentication/auth-email";
 import Link from "next/link";
 
-import './index.css';
 import { singIn } from "@/service/authentication";
+import { AuthEmailSchema } from "@/components/authentication/auth-email/auth-email-schema";
+import './index.css';
 
 export default function Email() {
+
+    async function sing({ email, password }: AuthEmailSchema) {
+        "use server"
+        await singIn({ email, password });
+    }
+
     return (
         <div className="container-login-email">
             <h1>
                 Entrar no Chat
             </h1>
 
-            <AuthEmail authType="singIn" action={singIn} />
+            <AuthEmail authType="singIn" action={sing} />
 
             <Link href={'/register'}>
                 Não possui conta? cadastre-se
--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -1,3 +1,5 @@
+import './index.css';
+
 export default function Layout({
     children,
   }: Readonly<{
