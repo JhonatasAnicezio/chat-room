b3bdded6ddd31af9f20f5a92690efa5332a8b590
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-21 12:36:00 -0300

    feat: add return initial page

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,13 +1,24 @@
+'use client'
+import Link from 'next/link';
 import './index.css';
+import { useUserStore } from '@/store/user-store';
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { user } = useUserStore(); 
+
   return (
       <div className={`login-layout`}>
+        <Link className='absolute top-7 left-7' href={'/'}>
+          {`<- Pagina Inicial`}
+        </Link>
         {children}
+        <button onClick={() => console.log(user)}>
+            Imprimir Usuario
+          </button>
       </div>
   );
 }
--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -1,14 +1,17 @@
+import Link from 'next/link';
 import './index.css';
 
 export default function Layout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <div className={`register-layout`}>
-          {children}
-        </div>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className={`register-layout`}>
+      <Link className='absolute top-7 left-7' href={'/'}>
+        {`<- Pagina Inicial`}
+      </Link>
+      {children}
+    </div>
+  );
+}
