f0064b33cbdfdc4ce0a97a45734167bdb4683872
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-23 13:05:58 -0300

    feat: implements UserProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import UserProvider from "@/context/User/user-provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} root-layout`}>
-        {children}
+        <UserProvider>
+          {children}
+        </UserProvider>
       </body>
     </html>
   );
