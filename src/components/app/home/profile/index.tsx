a7667a8e12c63c423a08a80f8c15f131e8affd81
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-23 19:54:49 -0300

    feat: implements css in profile

--- src/components/app/home/profile/index.tsx
+++ src/components/app/home/profile/index.tsx
@@ -3,7 +3,31 @@ import './index.css';
 export default function Profile() {
     return (
         <div className="container-profile">
+            <div className='banner' />
 
+            <div className='description'>
+                <div className='photo-user' />
+                <button className='button-edit'>
+                    Editar perfil
+                </button>
+
+                <div className='info'>
+                    <div className='info-children'>
+                        <div>
+                            <h3>NOME EXIBIDO</h3>
+                            <p>Photosynthesis</p>
+                        </div>
+                        <div>
+                            <h3>E-MAIL</h3>
+                            <p>jhonatasanic@hotmail.com</p>
+                        </div>
+                        <div>
+                            <h3>TELEFONE</h3>
+                            <p>(16)992219948</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
