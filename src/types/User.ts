18745aa8703661c1d36696cb8db77c0b04ecb131
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-23 20:20:14 -0300

    refact: destruct User response

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,13 +1,13 @@
 export interface User {
     token: string,
-    user: [
-        {
-            providerId: string,
-            uid: string,
-            displayName: string | null,
-            email: string,
-            phoneNumber: string | null,
-            photoURL: string | null
-        }
-    ]
+    user: [ Profile ]
+}
+
+export interface Profile {
+    providerId: string,
+    uid: string,
+    displayName: string | null,
+    email: string,
+    phoneNumber: string | null,
+    photoURL: string | null
 }
