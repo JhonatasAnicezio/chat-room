1c51d93700937dfc31f80f49f6e783e4c38e16b1
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-24 11:24:06 -0300

    feat: configure next for all hostnames

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**', // Permite qualquer domínio
+            },
+        ],
+    },
+};
 
 export default nextConfig;
