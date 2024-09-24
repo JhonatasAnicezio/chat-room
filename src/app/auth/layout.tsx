52bb3c3658de04ae1074d0ad2d74a3dd9233ddd1
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-24 11:38:56 -0300

    feat: create auth in singIn

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Link from 'next/link';
+import { useContext, useEffect } from 'react';
+import { UserContext } from '@/context/User/user-context';
+
+import './index.css';
+import { useRouter } from 'next/navigation';
+
+export default function Layout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    const { user } = useContext(UserContext);
+
+    if (user) {
+        useRouter().push('/');
+    }
+
+    return (
+        <div className={`auth-layout`}>
+            <Link className='absolute top-7 left-7' href={'/'}>
+                {`<- Pagina Inicial`}
+            </Link>
+            {children}
+        </div>
+    );
+}
