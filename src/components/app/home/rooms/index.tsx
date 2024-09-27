522dcab0ba59dfa6e562991868ad4d9bca40fd15
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-27 17:25:37 -0300

    feat: add styles

--- src/components/app/home/rooms/index.tsx
+++ src/components/app/home/rooms/index.tsx
@@ -1,5 +1,6 @@
 import findAllRooms from "@/service/rooms";
 import Link from "next/link";
+import './index.css';
 
 export default async function Rooms() {
     const rooms = await findAllRooms();
@@ -8,11 +9,11 @@ export default async function Rooms() {
         <>
             {rooms && rooms.map((room) => (
                 <Link key={room._id} href={`/chat/${room._id}`} className="div-room">
-                    <div className=""></div>
+                    <div className="banner-cover"></div>
 
                     <h2>{room.name}</h2>
 
-                    <div className="">
+                    <div className="div-subject">
                         {room.subject.map((e, index) => (
                             <span key={index}>{e}</span>
                         ))}
