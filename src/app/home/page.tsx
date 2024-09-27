2450fca7d0c041e4a212f3ee4fd49541843f904b
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-09-27 17:30:02 -0300

    feat: implements component Rooms

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,8 @@
-import Link from "next/link";
+import Rooms from '@/components/app/home/rooms';
 import './index.css';
 import Profile from "@/components/app/home/profile";
 
-export default function Home() {
-  const arr = new Array(1).fill(0);
-
+export default async function Home() {
   return (
     <main className="container-home">
       <div className="div-home">
@@ -14,17 +12,10 @@ export default function Home() {
           <div className="div-description">
             <h1>Salas de BatePapo</h1>
             <p>Venha conversar com pessoas que compartilham do mesmo interesse!</p>
-
           </div>
 
           <div className="rooms">
-            {arr.map((_e, index) => (
-              <Link key={index} href={'/chat/chatemconstrução'} className="flex flex-col gap-1 w-[19.5%]">
-                <div className="py-1 w-full h-[165px] bg-black"></div>
-
-                <h2 className="font-semibold">Sala aleatoria! Sala de bate papo aleatora</h2>
-              </Link>
-            ))}
+            <Rooms />
           </div>
         </section>
       </div>
